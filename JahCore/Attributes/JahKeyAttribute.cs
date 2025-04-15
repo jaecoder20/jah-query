c953fb51dadf5caa8e9ef9b1e9e05c9d5f7ff512
Rojae Wedderburn <rojaewedderburn@gmail.com>
2025-04-14 20:25:03 -0500

    Key attribute

---
+++ JahCore/Attributes/JahKeyAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace JahCore.Attributes;
+[AttributeUsage(AttributeTargets.Property)]
+public class JahKeyAttribute: System.Attribute
+{
+    public bool IsAutoIncrement { get; set; } = true;
+    // For composite keys, allow ordering
+    public int Order { get; set; }
+    public JahKeyAttribute()
+    {
+        // Default constructor
+    }
+}
